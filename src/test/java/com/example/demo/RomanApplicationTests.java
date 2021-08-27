8d43bd3649be1f38e58e1761d4d87751fa1243fb
Mario Thomas <PC@Mario>
2021-08-27 11:24:07 -0300

    initial commit 2

--- src/test/java/com/example/demo/RomanApplicationTests.java
+++ src/test/java/com/example/demo/RomanApplicationTests.java
@@ -13,18 +13,30 @@ class RomanApplicationTests {
     @Test
     public void testB() {
         var x = assertThrows(RuntimeException.class,
-                ()->convertToRomansString(0));
+                () -> convertToRomansString(0));
         assertTrue(x.getMessage().equals("Números menores a 1 no son válidos"));
+    }
 
+    @Test
+    public void testC() {
+        assertEquals("II", convertToRomansString(2));
+    }
 
+    @Test
+    public void testD() {
+        assertEquals("III", convertToRomansString(3));
     }
 
     private String convertToRomansString(int i) {
         if(i <= 0){
             throw new RuntimeException("Números menores a 1 no son válidos");
         }
-        return "I";
-    }
+        switch (i) {
+            case 1: return "I";
+            case 2: return "II";
+            default: return "III";
+        }
 
+    }
 
 }
