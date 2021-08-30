832f2f42f0843ba38e0e42da39847dafbf1880f9
Mario <executewithparams@gmail.com>
2021-08-29 23:16:09 -0300

    Witch only, slight performance boost

--- src/test/java/com/example/demo/RomanApplicationTests.java
+++ src/test/java/com/example/demo/RomanApplicationTests.java
@@ -93,73 +93,60 @@ public class RomanApplicationTests {
     }
 
     private String convertTenthToRomanString(int aNumberToConvert) {
-        StringBuilder romanString = new StringBuilder();
-        if (aNumberToConvert <= 3) {
-            for (int i = 0; i < aNumberToConvert; i++) {
-                romanString.append("X");
-            }
-        } else if (aNumberToConvert == 4) {
-            romanString.append("XL");
-        } else if (aNumberToConvert <= 8) {
-            romanString.append("L");
-            for (int i = 5; i < aNumberToConvert; i++) {
-                romanString.append("X");
-            }
-        } else {
-            romanString.append("XC");
+        switch (aNumberToConvert){
+            case 1: return "X";
+            case 2: return "XX";
+            case 3: return "XXX";
+            case 4: return "XL";
+            case 5: return "L";
+            case 6: return "LX";
+            case 7: return "LXX";
+            case 8: return "LXXX";
+            case 9: return "XC";
         }
-        return romanString.toString();
+        return "";
 
     }
 
     private String convertUnitToRomanString(int aNumberToConvert) {
-        StringBuilder romanString = new StringBuilder();
-        if (aNumberToConvert <= 3) {
-            for (int i = 0; i < aNumberToConvert; i++) {
-                romanString.append("I");
-            }
-        } else if (aNumberToConvert == 4) {
-            romanString.append("IV");
-        } else if (aNumberToConvert <= 8) {
-            romanString.append("V");
-            for (int i = 5; i < aNumberToConvert; i++) {
-                romanString.append("I");
-            }
-        } else {
-            romanString.append("IX");
+        switch (aNumberToConvert){
+            case 1: return "I";
+            case 2: return "II";
+            case 3: return "III";
+            case 4: return "IV";
+            case 5: return "V";
+            case 6: return "VI";
+            case 7: return "VII";
+            case 8: return "VIII";
+            case 9: return "IX";
         }
-        return romanString.toString();
+        return "";
 
     }
 
     private String convertCenthToRomanString(int aNumberToConvert) {
-        StringBuilder romanString = new StringBuilder();
-        if (aNumberToConvert <= 3) {
-            for (int i = 0; i < aNumberToConvert; i++) {
-                romanString.append("C");
-            }
-        } else if (aNumberToConvert == 4) {
-            romanString.append("CD");
-        } else if (aNumberToConvert <= 8) {
-            romanString.append("D");
-            for (int i = 5; i < aNumberToConvert; i++) {
-                romanString.append("C");
-            }
-        } else {
-            romanString.append("CM");
+        switch (aNumberToConvert){
+            case 1: return "C";
+            case 2: return "CC";
+            case 3: return "CCC";
+            case 4: return "CD";
+            case 5: return "D";
+            case 6: return "DC";
+            case 7: return "DCC";
+            case 8: return "DCCC";
+            case 9: return "CM";
         }
-        return romanString.toString();
+        return "";
 
     }
 
     private String convertThousanthToRomanString(int aNumberToConvert) {
-        StringBuilder romanString = new StringBuilder();
-        if (aNumberToConvert <= 3) {
-            for (int i = 0; i < aNumberToConvert; i++) {
-                romanString.append("M");
-            }
+        switch (aNumberToConvert){
+            case 1: return "M";
+            case 2: return "MM";
+            case 3: return "MMM";
         }
-        return romanString.toString();
+        return "";
 
     }
 }
